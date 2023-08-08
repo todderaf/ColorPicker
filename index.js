a11ffea710e2d06f6d11a944c026d4edc2835d9a
todderaf <79234395+todderaf@users.noreply.github.com>
2023-08-08 13:00:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const colorPicker = document.getElementById('color-picker');
 const submitBtn = document.getElementById('submit-btn');
 const dropdown = document.getElementById('dropdown');
- const showColorBox = document.getElementById('show-colors');
+const showColorBox = document.getElementById('show-colors');
 const pickerLabel = document.querySelector('.picker-label');
 //Retrieve the selected color from thecolorapi.com
 pickerLabel.textContent = colorPicker.value;
@@ -10,15 +10,18 @@ pickerLabel.textContent = colorPicker.value;
 function render(colors) {
   let i = 0;
   showColorBox.innerHTML = "";
- for (color of colors) {
+ for (const color of colors) {
    let colorValue = color.hex.value;
   showColorBox.innerHTML +=
-      `<div id=colorBox${i} class="color-box">
-        <span class="color-text">${colorValue}</span> 
+      `<div id="colorBox${i}"" class="color-box">
+        <span id="color-text${i}" class="color-text">${colorValue}</span> 
         </div>`; 
-   document.getElementById(`colorBox${i}`).style.backgroundColor = colorValue;
-   i++;  
+   document.getElementById(`colorBox${i}`).style.backgroundColor = colorValue;   
+  document.getElementById(`color-text${i}`).style.color =      
+       getTextColor(colorValue);
+    i++;  
  } 
+ 
 }
 
      //get colorPicker value when click submit button
@@ -37,3 +40,34 @@ submitBtn.addEventListener('click', (e) => {
 colorPicker.addEventListener('change', () => {
   pickerLabel.textContent = colorPicker.value;
 })
+
+function getRGB(c) {
+  return parseInt(c, 16) || c
+}
+
+function getsRGB(c) {
+  return getRGB(c) / 255 <= 0.03928
+    ? getRGB(c) / 255 / 12.92
+    : Math.pow((getRGB(c) / 255 + 0.055) / 1.055, 2.4)
+}
+
+function getLuminance(hexColor) {
+  return (
+    0.2126 * getsRGB(hexColor.substr(1, 2)) +
+    0.7152 * getsRGB(hexColor.substr(3, 2)) +
+    0.0722 * getsRGB(hexColor.substr(-2))
+  )
+}
+
+function getContrast(f, b) {
+  const L1 = getLuminance(f)
+  const L2 = getLuminance(b)
+  return (Math.max(L1, L2) + 0.05) / (Math.min(L1, L2) + 0.05)
+}
+
+function getTextColor(bgColor) {
+  const whiteContrast = getContrast(bgColor, '#ffffff')
+  const blackContrast = getContrast(bgColor, '#000000')
+
+  return whiteContrast > blackContrast ? '#ffffff' : '#000000'
+}
