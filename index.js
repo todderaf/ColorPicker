2a3d2b2838316b8a4d1a15f670d03f9a6b7ea457
todderaf <79234395+todderaf@users.noreply.github.com>
2023-08-08 10:53:35 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-
-const colorPicker = document.getElementById('color-picker')
-
+const colorPicker = document.getElementById('color-picker');
+const submitBtn = document.getElementById('submit-btn');
+const dropdown = document.getElementById('dropdown');
+ const showColorBox = document.getElementById('show-colors');
+const pickerLabel = document.querySelector('.picker-label');
 //Retrieve the selected color from thecolorapi.com
-fetch("https://www.thecolorapi.com/scheme?hex=ff0000&mode=complement&count=6")
-    .then(resp => resp.json())
-    .then(data => console.log(data.colors[1].hex.value)) /*data.colors is an array of objects hex, rgb, hsl,...etc*/
+pickerLabel.textContent = colorPicker.value;
+/*data.colors is an array of objects hex, rgb, hsl,...etc*/
 
+function render(colors) {
+  let i = 0;
+  showColorBox.innerHTML = "";
+ for (color of colors) {
+   let colorValue = color.hex.value;
+  showColorBox.innerHTML +=
+      `<div id=colorBox${i} class="color-box">
+        <span class="color-text">${colorValue}</span> 
+        </div>`; 
+   document.getElementById(`colorBox${i}`).style.backgroundColor = colorValue;
+   i++;  
+ } 
+}
 
      //get colorPicker value when click submit button
 
-
-     function selectOption() {
-        const dropdown = document.getElementById('dropdown');
-         // get the index of the selected option
-         const selectedIndex = dropdown.selectedIndex;
-        console.log(dropdown.options[selectedIndex].text)
-     }
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const colorPicked = colorPicker.value;
+  const dropdownValue = dropdown.options[dropdown.selectedIndex].value; 
+  fetchURL = `https://www.thecolorapi.com/scheme?hex=${colorPicked.substring(1)}&mode=${dropdownValue}&count=6`;
+  fetch(fetchURL)
+    .then(resp => resp.json())
+    // .then(data => alert(data.colors[1].hex.value));
+  .then(data => render(data.colors));
+ });
+     
+colorPicker.addEventListener('change', () => {
+  pickerLabel.textContent = colorPicker.value;
+})
